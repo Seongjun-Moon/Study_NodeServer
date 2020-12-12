7981a169a3008b2fe6d1a28604bb115a951c44f9
Seongjun-Moon <moonsj1128@gmail.com>
2020-12-12 19:30:09 +0900

    movie_data select, insert, update query

--- routers/movie/movieRouter.js
+++ routers/movie/movieRouter.js
@@ -46,42 +46,49 @@ router.post("/search", function (req, res, next) {
     //json으로 변환하여 app으로 전송
     // console.log(movie_data[0].movie);
 
-    const send_param = {
-      movie_name: movie_data[0].movie,
+    // const send_param = {
+    //   movie_name: movie_data[0].movie,
       // movie_url: url2 + img_data[0].movie_url,
-    };
+    // };
     // console.log(movie_data[0].movie);
-    // for (let i = 0; i < movie_data.length; i++) {
-    //   const send_param = {
-    //     movie_name: movie_data[i].movie,
-    //   };
-    //   axios
-    //     .post("http://localhost:7777/movieSearch/movie_search", send_param)
-    //     .then((res) => {
-    //       if (res.data.result.length != 0) {
-    //         console.log("이미 존재하는 영화");
-    //       } else {
-    //         axios.post(
-    //           "http://localhost:7777/movieSearch/movie_insert",
-    //           send_param
-    //         );
-    //       }
-    //     });
-    //   console.log(i);
-    // }
-    axios
-      .post("http://localhost:7777/movie/movieSearch", send_param)
-      .then((res) => {
-        //console.log(res.data.result);
-        if (res.data.result == null) {
-          // axios.post(
-          //   "http://localhost:7777/movieSearch/movieInsert",
-          //   send_param
-          // );
-        } else {
-          console.log("이미 존재하는 영화");
-        }
-      });
+    const update_data =[];
+    for (let i = 0; i < movie_data.length; i++) {
+      update_data.push( movie_data[i].movie);
+      const send_param = {
+        movie_name: movie_data[i].movie,
+      };
+      axios
+        .post("http://localhost:7777/movie/movieSearch", send_param)
+        .then((res) => {
+          if (res.data.result.length != 0) {
+            console.log("이미 존재하는 영화");
+          } else {
+            axios.post(
+              "http://localhost:7777/movie/movieInsert",
+              send_param
+            );
+          }
+        });
+      // console.log(i);
+    }
+    console.log(update_data);
+    axios.post(
+      "http://localhost:7777/movie/movieUpdate",
+      update_data
+    );
+    // axios
+    //   .post("http://localhost:7777/movie/movieSearch", send_param)
+    //   .then((res) => {
+    //     //console.log(res.data.result);
+    //     if (res.data.result.length == 0) {
+    //       axios.post(
+    //         "http://localhost:7777/movie/movieInsert",
+    //         send_param
+    //       );
+    //     } else {
+    //       console.log("이미 존재하는 영화");
+    //     }
+    //   });
     return res.json({ img_data, movie_data, movie });
   });
 });
@@ -94,20 +101,27 @@ router.post("/movieSearch", (req, res) => {
       res.json({ message: false });
     } else {
       res.json({ result: result });
-      console.log(result);
-      console.log("movie record find");
+      if(result.length==0){
+        
+      }else{
+        console.log(result);
+        console.log("movie record find");
+
+      }
     }
   });
 });
 
 router.post("/movieInsert", (req, res) => {
   const movie_nm = req.body.movie_name;
+  const use_yn="Y";
   // const movie_url = req.body.movie_url;
-  var sql = "insert into movie_info(movie_nm) values (?)";
-  console.log(sql);
-  con.query(sql, [movie_nm], function (err, result) {
+  var sql = "insert into movie_info(movie_nm, use_yn) values (?, ?)";
+  // console.log(sql);
+  con.query(sql, [movie_nm, use_yn], function (err, result) {
     if (err) {
       res.json({ message: false });
+      console.log(err);
     } else {
       res.json({ message: movie_nm });
       console.log("movie record inserted");
@@ -115,4 +129,20 @@ router.post("/movieInsert", (req, res) => {
   });
 });
 
+router.post("/movieUpdate", (req, res) => {
+  const movie_query = req.body;
+  console.log(movie_query);
+  // const movie_url = req.body.movie_url;
+  var sql = "update movie_info set use_yn='N' where movie_nm not in (?)";
+  // console.log(sql);
+  con.query(sql, [movie_query], function (err, result) {
+    if (err) {
+      res.json({ message: false });
+      console.log(err);
+    } else {
+      res.json({ message: true });
+      console.log("movie record update");
+    }
+  });
+});
 module.exports = router;
